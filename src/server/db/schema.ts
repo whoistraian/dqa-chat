dca0aa7e083d5b9f1bc57b0407ced99975007d8d
Traian Simedru <traians1@outlook.com>
2024-07-06 20:32:58 +0300

    :sparkles: feat: data modeling

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,193 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
+  pgTable,
+  primaryKey,
+  text,
   timestamp,
-  varchar,
+  uuid,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `fluffy-happiness_${name}`);
+export const users = pgTable("users", {
+  id: uuid("id").unique().defaultRandom().primaryKey(),
+  email: text("email").unique().notNull(),
+  password: text("password").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  created_by_id: uuid("created_by_id").notNull(),
+  updated_by_id: uuid("updated_by_id").notNull(),
+});
 
-export const posts = createTable(
-  "post",
+export const usersRelations = relations(users, ({ one, many }) => ({
+  roles: many(roles),
+  userCreatedBy: one(users, {
+    fields: [users.created_by_id],
+    references: [users.id],
+  }),
+  userUpdatedBy: one(users, {
+    fields: [users.updated_by_id],
+    references: [users.id],
+  }),
+}));
+
+export const roles = pgTable("roles", {
+  id: uuid("id").unique().defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  user_id: uuid("user_id"),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  created_by_id: uuid("created_by_id").notNull(),
+  updated_by_id: uuid("updated_by_id").notNull(),
+});
+
+export const rolesRelations = relations(roles, ({ one, many }) => ({
+  users: one(users, {
+    fields: [roles.user_id],
+    references: [users.id],
+  }),
+  roleCreatedBy: one(users, {
+    fields: [roles.created_by_id],
+    references: [users.id],
+  }),
+  roleUpdatedBy: one(users, {
+    fields: [roles.updated_by_id],
+    references: [users.id],
+  }),
+  rolesToPermissions: many(rolesToPermissions),
+}));
+
+export const permissions = pgTable("permissions", {
+  id: uuid("id").unique().defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  created_by_id: uuid("created_by_id").notNull(),
+  updated_by_id: uuid("updated_by_id").notNull(),
+});
+
+export const permissionsRelations = relations(permissions, ({ many }) => ({
+  rolesToPermissions: many(rolesToPermissions),
+}));
+
+export const rolesToPermissions = pgTable(
+  "roles_to_permissions",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    role_id: uuid("role_id")
+      .notNull()
+      .references(() => roles.id),
+    permission_id: uuid("permission_id")
+      .notNull()
+      .references(() => permissions.id),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (t) => ({
+    pk: primaryKey({ columns: [t.role_id, t.permission_id] }),
+  }),
+);
+
+export const rolesToPermissionsRelations = relations(
+  rolesToPermissions,
+  ({ one }) => ({
+    role: one(roles, {
+      fields: [rolesToPermissions.role_id],
+      references: [roles.id],
+    }),
+    permission: one(permissions, {
+      fields: [rolesToPermissions.permission_id],
+      references: [permissions.id],
+    }),
+  }),
+);
+
+export const threads = pgTable("threads", {
+  id: uuid("id").unique().defaultRandom().primaryKey(),
+  title: text("title").notNull(),
+  user_id: uuid("user_id").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  created_by_id: uuid("created_by_id").notNull(),
+  updated_by_id: uuid("updated_by_id").notNull(),
+});
+
+export const threadsRelations = relations(threads, ({ one }) => ({
+  user: one(users, {
+    fields: [threads.user_id],
+    references: [users.id],
+  }),
+  threadCreatedBy: one(users, {
+    fields: [threads.created_by_id],
+    references: [users.id],
+  }),
+  threadUpdatedBy: one(users, {
+    fields: [threads.updated_by_id],
+    references: [users.id],
+  }),
+}));
+
+export const messages = pgTable("messages", {
+  id: uuid("id").unique().defaultRandom().primaryKey(),
+  thread_id: uuid("thread_id").notNull(),
+  content: text("content").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  created_by_id: uuid("created_by_id").notNull(),
+  updated_by_id: uuid("updated_by_id").notNull(),
+});
+
+export const messagesRelations = relations(messages, ({ one, many }) => ({
+  thread: one(threads, {
+    fields: [messages.thread_id],
+    references: [threads.id],
+  }),
+  messageCreatedBy: one(users, {
+    fields: [messages.created_by_id],
+    references: [users.id],
+  }),
+  messageUpdatedBy: one(users, {
+    fields: [messages.updated_by_id],
+    references: [users.id],
+  }),
+  messages: many(messages),
+}));
+
+export const threadsToMessages = pgTable(
+  "threads_to_messages",
+  {
+    thread_id: uuid("thread_id")
+      .notNull()
+      .references(() => threads.id),
+    message_id: uuid("message_id")
+      .notNull()
+      .references(() => messages.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.thread_id, t.message_id] }),
+  }),
+);
+
+export const threadsToMessagesRelations = relations(
+  threadsToMessages,
+  ({ one }) => ({
+    thread: one(threads, {
+      fields: [threadsToMessages.thread_id],
+      references: [threads.id],
+    }),
+    message: one(messages, {
+      fields: [threadsToMessages.message_id],
+      references: [messages.id],
+    }),
+  }),
 );
